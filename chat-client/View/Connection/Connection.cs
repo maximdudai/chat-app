6a456f88c94a3eb445caee422b69aa885984a58c
maximdudai <maxim.dudai01@gmail.com>
2024-04-14 20:45:54 +0100

    Add project files.

---
+++ chat-client/View/Connection/Connection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace chat_client
+{
+    public partial class chatApp : Form
+    {
+        public chatApp()
+        {
+            InitializeComponent();
+
+            // prevent resizing of form
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+        }
+    }
+}
