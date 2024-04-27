5cef009b70e5a5d4022ce9d2fa191e2d5a994eca
Edo Santos <2231440@my.ipleiria.pt>
2024-04-27 20:42:33 +0100

    Chat
    - chat ui

--- chat-client/View/Chat/Chat.Designer.cs
+++ chat-client/View/Chat/Chat.Designer.cs
@@ -29,51 +29,19 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Chat));
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.exitPictureBox = new System.Windows.Forms.PictureBox();
-            this.sendMessagePictureBox = new System.Windows.Forms.PictureBox();
             this.chatConnectionListBox = new System.Windows.Forms.ListBox();
             this.chatMessageListBox = new System.Windows.Forms.ListBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.messageTextBox = new System.Windows.Forms.TextBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.exitPictureBox)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.sendMessagePictureBox)).BeginInit();
+            this.sendButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.emojiButton = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(158, -29);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(170, 171);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            // 
-            // exitPictureBox
-            // 
-            this.exitPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("exitPictureBox.Image")));
-            this.exitPictureBox.Location = new System.Drawing.Point(428, 5);
-            this.exitPictureBox.Name = "exitPictureBox";
-            this.exitPictureBox.Size = new System.Drawing.Size(67, 37);
-            this.exitPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.exitPictureBox.TabIndex = 1;
-            this.exitPictureBox.TabStop = false;
-            // 
-            // sendMessagePictureBox
-            // 
-            this.sendMessagePictureBox.BackColor = System.Drawing.SystemColors.ScrollBar;
-            this.sendMessagePictureBox.Image = ((System.Drawing.Image)(resources.GetObject("sendMessagePictureBox.Image")));
-            this.sendMessagePictureBox.Location = new System.Drawing.Point(421, 489);
-            this.sendMessagePictureBox.Name = "sendMessagePictureBox";
-            this.sendMessagePictureBox.Size = new System.Drawing.Size(35, 30);
-            this.sendMessagePictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.sendMessagePictureBox.TabIndex = 2;
-            this.sendMessagePictureBox.TabStop = false;
             // chatConnectionListBox
             // 
             this.chatConnectionListBox.BackColor = System.Drawing.SystemColors.ScrollBar;
@@ -92,9 +60,9 @@
             this.chatMessageListBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.chatMessageListBox.FormattingEnabled = true;
             this.chatMessageListBox.ItemHeight = 16;
-            this.chatMessageListBox.Location = new System.Drawing.Point(28, 275);
+            this.chatMessageListBox.Location = new System.Drawing.Point(28, 264);
             this.chatMessageListBox.Name = "chatMessageListBox";
-            this.chatMessageListBox.Size = new System.Drawing.Size(428, 208);
+            this.chatMessageListBox.Size = new System.Drawing.Size(428, 224);
             this.chatMessageListBox.TabIndex = 4;
             // 
             // label1
@@ -106,18 +74,6 @@
             this.label1.Size = new System.Drawing.Size(206, 39);
             this.label1.TabIndex = 6;
             this.label1.Text = "Chat Instatly\r\n";
-            // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.SystemColors.ScrollBar;
-            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
-            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(394, 488);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(30, 31);
-            this.button1.TabIndex = 7;
-            this.button1.UseVisualStyleBackColor = false;
             // 
             // label2
             // 
@@ -146,8 +102,50 @@
             this.messageTextBox.Location = new System.Drawing.Point(28, 489);
             this.messageTextBox.Multiline = true;
             this.messageTextBox.Name = "messageTextBox";
-            this.messageTextBox.Size = new System.Drawing.Size(367, 30);
+            this.messageTextBox.Size = new System.Drawing.Size(359, 30);
             this.messageTextBox.TabIndex = 5;
+            // 
+            // sendButton
+            // 
+            this.sendButton.BackgroundImage = global::chat_client.Properties.Resources.send;
+            this.sendButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.sendButton.Location = new System.Drawing.Point(420, 488);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(34, 32);
+            this.sendButton.TabIndex = 17;
+            this.sendButton.UseVisualStyleBackColor = true;
+            this.sendButton.Click += new System.EventHandler(this.sendButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.BackgroundImage = global::chat_client.Properties.Resources.logout;
+            this.exitButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.exitButton.Location = new System.Drawing.Point(450, 4);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(32, 32);
+            this.exitButton.TabIndex = 16;
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.handleLoginAccount);
+            // 
+            // emojiButton
+            // 
+            this.emojiButton.BackgroundImage = global::chat_client.Properties.Resources.emoji;
+            this.emojiButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.emojiButton.Location = new System.Drawing.Point(388, 488);
+            this.emojiButton.Name = "emojiButton";
+            this.emojiButton.Size = new System.Drawing.Size(32, 32);
+            this.emojiButton.TabIndex = 15;
+            this.emojiButton.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::chat_client.Properties.Resources.chat_app;
+            this.pictureBox3.Location = new System.Drawing.Point(117, -23);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(257, 162);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 12;
+            this.pictureBox3.TabStop = false;
             // 
             // Chat
             // 
@@ -155,22 +153,20 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.InactiveCaption;
             this.ClientSize = new System.Drawing.Size(484, 561);
-            this.Controls.Add(this.messageTextBox);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.sendMessagePictureBox);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.sendButton);
+            this.Controls.Add(this.exitButton);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.emojiButton);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.messageTextBox);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.chatMessageListBox);
             this.Controls.Add(this.chatConnectionListBox);
-            this.Controls.Add(this.exitPictureBox);
-            this.Controls.Add(this.pictureBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Chat";
             this.Text = "Chat";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.exitPictureBox)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.sendMessagePictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -178,15 +174,19 @@
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox exitPictureBox;
-        private System.Windows.Forms.PictureBox sendMessagePictureBox;
+        //private System.Windows.Forms.PictureBox pictureBox1;
+        //private System.Windows.Forms.PictureBox exitPictureBox;
+        //private System.Windows.Forms.PictureBox sendMessagePictureBox;
         private System.Windows.Forms.ListBox chatConnectionListBox;
         private System.Windows.Forms.ListBox chatMessageListBox;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
+        //private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox messageTextBox;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button emojiButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button sendButton;
     }
 }
